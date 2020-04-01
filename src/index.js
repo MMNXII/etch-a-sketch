172bf056eb6d772303755d9a0238d7ab0f089b5e
Matthew Neal <matthewneal1202@gmail.com>
2020-03-31 19:08:52 -0700

    modularize functions and move all to JS

--- src/index.js
+++ src/index.js
@@ -1,61 +1,110 @@
 import './index.css';
+import anime from '../node_modules/animejs/lib/anime.es.js';
+
 
 (function () {
 
-  const sketchContainer = document.getElementById("sketchContainer");
+  let gridItem;
+  let gridItemArr = [];
 
-  function etch() {
-    for (var a = 0; a < 4096; a++) {
 
-      let div = document.createElement("div");
-        div.className = "gridItem";
-        sketchContainer.appendChild(div);
+  (function createTitle () {
+    var title = document.createElement("h1");
+        title.textContent = "Etch-A-Sketch";
+        title.id = "title";
 
-      let colorButton = document.getElementById("colorBtn");
-        colorButton.addEventListener("click", color);
+        document.body.appendChild(title);
+  })();
 
-        function color() {
-          div.addEventListener("mouseover", getRandomColor);
 
-            function getRandomColor() {
-              let letters = "0123456789ABCDEF";
-              let color = "#";
-                for (var i = 0; i < 6; i++) {
-                  color += letters[Math.floor(Math.random() * 16)];
-                }
-                  if (color == "#000000") {
-                    div.removeEventListener("mouseover", getRandomColor);
-                } if (div.style.backgroundColor != "#FFFFFF") {
-                    div.removeEventListener("mouseover", getRandomColor);
+  (function createSketchPad () {
+
+    let sketchDiv = document.createElement("div");
+        sketchDiv.id = "sketchContainer";
+        document.body.appendChild(sketchDiv);
+
+      for (var a = 0; a < 4096; a++) {
+
+        gridItem = document.createElement("div");
+        gridItem.className = "gridItem";
+        gridItemArr.push(gridItem);
+        sketchDiv.appendChild(gridItem);
+      }
+
+  })();
+
+
+  (function colorBwBtn () {
+    let btnDiv = document.createElement("div");
+        btnDiv.id = "buttonsDiv";
+        document.body.appendChild(btnDiv);
+
+
+    let colorButton = document.createElement("button");
+        colorButton.textContent = "Color";
+        colorButton.id = "colorBtn";
+          btnDiv.appendChild(colorButton);
+          
+    let bwButton = document.createElement("button");
+        bwButton.textContent = "Black and White";
+        bwButton.id = "bwBtn";
+          btnDiv.appendChild(bwButton);
+
+  
+    let resetbtn = document.createElement("button");
+        resetbtn.textContent = "Reset";
+        resetbtn.id = "reset";
+          btnDiv.appendChild(resetbtn);
+
+          colorButton.addEventListener("click", color);
+
+          function color() {
+            gridItemArr.forEach(grid => {
+              grid.addEventListener("mouseover", getRandomColor);
+
+              function getRandomColor() {
+                let letters = "0123456789ABCDEF";
+                let color = "#";
+                  for (var i = 0; i < 6; i++) {
+                    color += letters[Math.floor(Math.random() * 16)];
                   }
-                    div.style.backgroundColor = color;
+                    if (color == "#000000") {
+                      grid.removeEventListener("mouseover", getRandomColor);
+                  } if (grid.style.backgroundColor != "#FFFFFF") {
+                      grid.removeEventListener("mouseover", getRandomColor);
+                  }
+                      grid.style.backgroundColor = color;
+              };
+            });
+          }
+
+          bwButton.addEventListener("click", bw);
+          
+          function bw() {
+            gridItemArr.forEach(grid => {
+              grid.addEventListener("mouseover", bandw);
+
+              function bandw() {
+                grid.style.backgroundColor = "#000000";
               }
+            })
+          };
+
+          resetbtn.addEventListener("click", resetSketch)
+
+          function resetSketch() {
+
+              grid.style.backgroundColor = "#FFFF"
             }
 
-      let blackAndWhite = document.getElementById("blackAndWhiteBtn");
-        blackAndWhite.addEventListener("click", bw);
+          
 
-        function bw() {
-          div.addEventListener("mouseover", bandw);
 
-          function bandw() {
-            div.style.backgroundColor = "#000000";
-          }
-        }
-  
-    
-    let reset = document.getElementById("reset");
-      reset.addEventListener("click", resetSketch);
+  })();
 
-      function resetSketch() {
-        div.style.backgroundColor = "#FFFFFF";
 
-      }
-    }
-  }
+})();
 
-  etch();
 
-})()
 
 
