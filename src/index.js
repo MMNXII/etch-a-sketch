d453e71cfc615a42b69827eed987994d05620f0d
Matthew Neal <matthewneal1202@gmail.com>
2020-04-07 12:13:09 -0700

    linter added

--- src/index.js
+++ src/index.js
@@ -1,199 +1,183 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-param-reassign */
 import './index.css';
+// eslint-disable-next-line import/extensions
 import anime from '../node_modules/animejs/lib/anime.es.js';
 
 
-(function () {
-
+(function etchASketch() {
   let gridItem;
-  let gridItemArr = [];
+  const gridItemArr = [];
 
 
-  (function createTitle () {
-    var title = document.createElement("h1");
-        title.textContent = "Etch-A-Sketch";
-        title.id = "title";
+  (function createTitle() {
+    const title = document.createElement('h1');
+    title.textContent = 'Etch-A-Sketch';
+    title.id = 'title';
 
-        document.body.appendChild(title);
-  })();
-
-
-  (function createSketchPad () {
-
-    let sketchDiv = document.createElement("div");
-        sketchDiv.id = "sketchContainer";
-        document.body.appendChild(sketchDiv);
-
-      for (var a = 0; a < 4096; a++) {
-
-        gridItem = document.createElement("div");
-        gridItem.className = "gridItem";
-        gridItemArr.push(gridItem);
-        sketchDiv.appendChild(gridItem);
-      }
-
-  })();
+    document.body.appendChild(title);
+  }());
 
 
-  (function colorBwBtn () {
-    let btnDiv = document.createElement("div");
-        btnDiv.id = "buttonsDiv";
-        document.body.appendChild(btnDiv);
+  (function createSketchPad() {
+    const sketchDiv = document.createElement('div');
+    sketchDiv.id = 'sketchContainer';
+    document.body.appendChild(sketchDiv);
+
+    for (let a = 0; a < 4096; a += 1) {
+      gridItem = document.createElement('div');
+      gridItem.className = 'gridItem';
+      gridItemArr.push(gridItem);
+      sketchDiv.appendChild(gridItem);
+    }
+  }());
 
 
-    let colorButton = document.createElement("button");
-        colorButton.textContent = "Color";
-        colorButton.id = "colorBtn";
-          btnDiv.appendChild(colorButton);
-          
-    let bwButton = document.createElement("button");
-        bwButton.textContent = "Black";
-        bwButton.id = "bwBtn";
-          btnDiv.appendChild(bwButton);
+  (function colorBwBtn() {
+    const btnDiv = document.createElement('div');
+    btnDiv.id = 'buttonsDiv';
+    document.body.appendChild(btnDiv);
 
-  
-    let resetbtn = document.createElement("button");
-        resetbtn.textContent = "Reset";
-        resetbtn.id = "reset";
-          btnDiv.appendChild(resetbtn);
 
-          colorButton.addEventListener("click", color);
+    const colorButton = document.createElement('button');
+    colorButton.textContent = 'Color';
+    colorButton.id = 'colorBtn';
+    btnDiv.appendChild(colorButton);
 
-          function color() {
+    const bwButton = document.createElement('button');
+    bwButton.textContent = 'Black';
+    bwButton.id = 'bwBtn';
+    btnDiv.appendChild(bwButton);
 
-            anime({
-              targets: colorButton,
-              translateX: {
-                value: '*=1.5',
-                duration: 500
-              },
-              width: {
-                value: '+=10px',
-                duration: 500,
-                easing: 'easeInOutSine'
-              },
-              rotate: {
-                value: '+=1turn',
-                duration: 500,
-                easing: 'easeInOutSine'
-              },
-              direction: 'alternate'
-            });
 
-            gridItemArr.forEach(grid => {
-              grid.addEventListener("mouseover", getRandomColor);
+    const resetbtn = document.createElement('button');
+    resetbtn.textContent = 'Reset';
+    resetbtn.id = 'reset';
+    btnDiv.appendChild(resetbtn);
 
-              function getRandomColor() {
-                let letters = "0123456789ABCDEF";
-                let color = "#";
-                  for (var i = 0; i < 6; i++) {
-                    color += letters[Math.floor(Math.random() * 16)];
-                  }
-                    if (color == "#000000") {
-                      grid.removeEventListener("mouseover", getRandomColor);
-                  } if (grid.style.backgroundColor != "#FFFFFF") {
-                      grid.removeEventListener("mouseover", getRandomColor);
-                  }
-                      grid.style.backgroundColor = color;
-              };
-            });
+    function color() {
+      anime({
+        targets: colorButton,
+        translateX: {
+          value: '*=1.5',
+          duration: 500,
+        },
+        width: {
+          value: '+=10px',
+          duration: 500,
+          easing: 'easeInOutSine',
+        },
+        rotate: {
+          value: '+=1turn',
+          duration: 500,
+          easing: 'easeInOutSine',
+        },
+        direction: 'alternate',
+      });
+
+      gridItemArr.forEach((grid) => {
+        grid.addEventListener('mouseover', getRandomColor);
+
+        function getRandomColor() {
+          const letters = '0123456789ABCDEF';
+          let hexNum = '#';
+          for (let i = 0; i < 6; i += 1) {
+            hexNum += letters[Math.floor(Math.random() * 16)];
           }
-
-          bwButton.addEventListener("click", bw);
-          
-          function bw() {
-
-            anime({
-              targets: bwButton,
-              translateX: {
-                value: '*=1.5',
-                duration: 500
-              },
-              width: {
-                value: '+=10px',
-                duration: 500,
-                easing: 'easeInOutSine'
-              },
-              rotate: {
-                value: '+=1turn',
-                duration: 500,
-                easing: 'easeInOutSine'
-              },
-              direction: 'alternate'
-            });
-
-            gridItemArr.forEach(grid => {
-              grid.addEventListener("mouseover", bandw);
-
-              function bandw() {
-                grid.style.backgroundColor = "#000000";
-              }
-            })
-          };
-
-          resetbtn.addEventListener("click", resetSketch)
-
-          function resetSketch() {
-
-            anime({
-              targets: resetbtn,
-              width: {
-                value: '+=10px',
-                duration: 500,
-                easing: 'easeInOutSine'
-              },
-              rotate: {
-                value: '+=1turn',
-                duration: 500,
-                easing: 'easeInOutSine'
-              },
-              direction: 'alternate'
-            });
-
-            gridItemArr.forEach(grid => {
-           
-              grid.style.backgroundColor = "#FFFF"
-              
-            })
+          if (hexNum === '#000000') {
+            grid.removeEventListener('mouseover', getRandomColor);
+          } if (grid.style.backgroundColor !== '#FFFFFF') {
+            grid.removeEventListener('mouseover', getRandomColor);
           }
+          grid.style.backgroundColor = hexNum;
+        }
+      });
+    }
 
-          
+    colorButton.addEventListener('click', color);
 
+    function bw() {
+      anime({
+        targets: bwButton,
+        translateX: {
+          value: '*=1.5',
+          duration: 500,
+        },
+        width: {
+          value: '+=10px',
+          duration: 500,
+          easing: 'easeInOutSine',
+        },
+        rotate: {
+          value: '+=1turn',
+          duration: 500,
+          easing: 'easeInOutSine',
+        },
+        direction: 'alternate',
+      });
 
-  })();
+      gridItemArr.forEach((grid) => {
+        grid.addEventListener('mouseover', bandw);
+
+        function bandw() {
+          grid.style.backgroundColor = '#000000';
+        }
+      });
+    }
+
+    bwButton.addEventListener('click', bw);
+
+    function resetSketch() {
+      anime({
+        targets: resetbtn,
+        width: {
+          value: '+=10px',
+          duration: 500,
+          easing: 'easeInOutSine',
+        },
+        rotate: {
+          value: '+=1turn',
+          duration: 500,
+          easing: 'easeInOutSine',
+        },
+        direction: 'alternate',
+      });
+
+      gridItemArr.forEach((grid) => {
+        grid.style.backgroundColor = '#FFFF';
+      });
+    }
+
+    resetbtn.addEventListener('click', resetSketch);
+  }());
 
   anime({
-    targets: "#title",
+    targets: '#title',
     duration: 1500,
-    delay: anime.stagger(500, { start: 300, from: "first" }),
+    delay: anime.stagger(500, { start: 300, from: 'first' }),
     opacity: [0, 1],
-    translateY: ["-1rem", 0],
-    direction: "normal",
+    translateY: ['-1rem', 0],
+    direction: 'normal',
 
   });
 
   anime({
-    targets: "#sketchContainer",
+    targets: '#sketchContainer',
     duration: 1500,
     delay: 700,
     opacity: [0, 1],
-    translateY: ["2rem", 0],
-    direction: "normal",
+    translateY: ['2rem', 0],
+    direction: 'normal',
 
   });
 
   anime({
-    targets: "#buttonsDiv",
+    targets: '#buttonsDiv',
     duration: 1500,
     delay: 1400,
     opacity: [0, 1],
-    translateY: ["-1rem", 0],
-    direction: "normal",
+    translateY: ['-1rem', 0],
+    direction: 'normal',
 
   });
-
-
-})();
-
-
-
-
+}());
